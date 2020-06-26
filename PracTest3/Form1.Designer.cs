7cc40f685bcce926edf87c6b3c249e52d0878d80
Nepcam <cameron.nepe@gmail.com>
2020-06-26 13:46:05 +1200

    Add project files.

---
+++ PracTest3/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PracTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxMap = new System.Windows.Forms.PictureBox();
+            this.listBoxData = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMap
+            // 
+            this.pictureBoxMap.BackgroundImage = global::PracTest2.Properties.Resources.s14;
+            this.pictureBoxMap.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBoxMap.InitialImage = null;
+            this.pictureBoxMap.Location = new System.Drawing.Point(0, 163);
+            this.pictureBoxMap.Name = "pictureBoxMap";
+            this.pictureBoxMap.Size = new System.Drawing.Size(730, 556);
+            this.pictureBoxMap.TabIndex = 3;
+            this.pictureBoxMap.TabStop = false;
+            // 
+            // listBoxData
+            // 
+            this.listBoxData.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxData.FormattingEnabled = true;
+            this.listBoxData.ItemHeight = 14;
+            this.listBoxData.Location = new System.Drawing.Point(0, 27);
+            this.listBoxData.Name = "listBoxData";
+            this.listBoxData.Size = new System.Drawing.Size(730, 130);
+            this.listBoxData.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 719);
+            this.Controls.Add(this.listBoxData);
+            this.Controls.Add(this.pictureBoxMap);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "103 Practical Test 3 ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMap;
+        private System.Windows.Forms.ListBox listBoxData;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+    }
+}
+
